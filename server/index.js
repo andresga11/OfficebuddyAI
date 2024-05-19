eb807b96ece9bfda7448fcedceb6a8100bd44586
andresga11 <69830280+andresga11@users.noreply.github.com>
2024-05-18 22:07:06 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,7 @@
-// const express = require("express");
-// const cors = require("cors");
-// const sqlite3 = require("sqlite3").verbose();
-// const userRoute = require("./userRoute");
-// const vectordbRoute = require("./vectordbRoute.js");
 import express from "express";
 import cors from "cors";
-// import sqlite3 from "sqlite3";
 import userRoute from "./userRoute.js";
 import vectordbRoute from "./vectordbRoute.js";
-
-// import mysqlRoute from "./mysqlServer.js"; 
 
 const app = express();
 app.use(cors());
@@ -20,14 +12,6 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-// app.use(express.json({ limit: "10mb" }));
-
-// let db = new sqlite3.Database("obai.db", (err) => {
-//   if (err) {
-//     console.error(err.message);
-//   }
-//   console.log("Connected to the access database.");
-// });
 
 app.get("/", function (req,res) {
   res.sendFile(
@@ -46,4 +30,3 @@ app.listen(PORT, () => console.log("listening at port " + PORT));
 
 app.use("/api/user", userRoute);
 app.use("/api/vectordb", vectordbRoute);
-// app.use("/api/mysql",mysqlRoute);
